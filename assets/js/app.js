590bb7036f0947ec748a728f8fdb27d6c641cad7
Vinicius Honorato <viniciushs2510@gmail.com>
2023-08-23 17:09:09 -0300

    adding js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,23 @@
+const pokemonList = document.getElementById('pokemonList');
 
-console.log('Hello World!');
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">Grass</li>
+            <li class="type">Poison</li>
+        </ol>
+        <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}"/>
+    </div>
+    </li>
+    `;
+}
+
+pokeApi.getPokemon(0, 10).then((pokemons = []) => {
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('');
+});
---
+++ assets/js/poke-api.js
@@ -0,0 +1,12 @@
+
+
+const pokeApi = {}
+
+pokeApi.getPokemon = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.results)
+        .catch((error) => console.error(error));
+}
